695c88c3c901521ce6e968e5c83ce7e188bd9818
Divya Pant <60366515+DivyaPant@users.noreply.github.com>
2023-05-03 12:11:38 +0530

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,33 @@
+function game(){
+var randomNumber1= Math.random();
+randomNumber1=Math.floor(randomNumber1*6)+1;
+
+var randomImage1= "dice"+randomNumber1+".png"
+
+document.querySelector(".dice .img1").setAttribute("src", randomImage1);
+
+
+var randomNumber2= Math.random();
+randomNumber2=Math.floor(randomNumber2*6)+1;
+
+var randomImage2= "dice"+randomNumber2+".png"
+
+document.querySelector(".dice .img2").setAttribute("src", randomImage2);
+
+
+if (randomNumber1>randomNumber2) {
+
+	document.querySelector("h1").innerHTML= "🚩 Player 1 Wins!";
+}
+
+else if(randomNumber1<randomNumber2) {
+
+	document.querySelector("h1").innerHTML= "Player 2 Wins! 🚩";
+}
+
+else {
+
+	document.querySelector("h1").innerHTML= "It's a draw! Try Again!!!";
+}
+
+}
